92cb7169320bfb6400c53431378b3cdb7c2cce02
Haroon Ali <haroonchlearn2earn@gmail.com>
2022-12-09 20:25:07 -0800

    update query

--- includes/dboperation.php
+++ includes/dboperation.php
@@ -12,7 +12,7 @@ class DbOperation
         $this->con = $db->connect();
     }
 
-
+    // insertimage
     function insert_Profile($name, $email,$image)
     {
     date_default_timezone_set("America/Los_Angeles");
@@ -30,44 +30,8 @@ class DbOperation
         return PROFILE_NOT_CREATED;
     }
 
-
-
-    // function update_Profile($id,$name, $email,$image)
-    // {
-    // date_default_timezone_set("America/Los_Angeles");
-    //            $time = date("ymd");
-    //            $id = '_' . $time . '_' . microtime(true);
-    //            $upload_path = "../images/$id.jpg";
-    //            $upload = substr($upload_path, 3);
-    //     $stmt=$this->con->prepare("UPDATE `ptest` SET `name`= ? ,`email`=?,`image`=? WHERE id = ?");
-    //     $stmt->bind_param("sssi", $name, $email,$upload, $id);
-    //     if ($stmt->execute())
-    //     {
-    //     file_put_contents($upload_path, base64_decode($image));
-    //         return PROFILE_CREATED;
-    //     }
-    //     return PROFILE_NOT_CREATED;
-    // }
-
     
-// function update_Profile($id, $name, $email, $image)
-// {
-//     if (isset($image)) {
-//         date_default_timezone_set("Asia/Karachi");
-//         $time = date("ymd");
-//         $id = $time . '_' . microtime(true);
-//         $upload_path = "../images/$id.jpg";
-//         $upload = substr($upload_path, 3);
-//     } else
-//         $upload_path = null;
-//     $stmt = $this->con->prepare("UPDATE `ptest` SET `name`=?,`email`=?,`image`=?  WHERE `id`= ?");
-//     $stmt->bind_param("sssi", $name, $email, $upload, $id);
-//     if ($stmt->execute()) {
-//         file_put_contents($upload_path, base64_decode($image));
-//         return PROFILE_UPDATED;
-//     }
-//     return PROFILE_NOT_UPDATED;
-// }
+
 
 
 function update_profile($id, $name, $email, $image)
